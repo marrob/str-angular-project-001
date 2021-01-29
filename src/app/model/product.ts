1d7c235bea4cf46d23a38b4b14ce591f9ee6b018
janosinori <janosinori@gmail.com>
2021-01-29 13:55:36 +0100

    Removed gets and sets

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -1,6 +1,6 @@
 interface IProduct {
-    id: string;
-    catId: string;
+    id: number;
+    catId: number;
     name: string;
     description: string;
     image: string;
@@ -9,30 +9,30 @@ interface IProduct {
     featured: boolean;
     active: boolean;
 
-    getId(): string;
-    getCatId(): string;
-    getName(): string;
-    getDescription(): string;
-    getImage(): string;
-    getPrice(): number;
-    getStock(): number;
-    isFeatured(): boolean;
-    isActive(): boolean;
+    // getId(): string;
+    // getCatId(): string;
+    // getName(): string;
+    // getDescription(): string;
+    // getImage(): string;
+    // getPrice(): number;
+    // getStock(): number;
+    // isFeatured(): boolean;
+    // isActive(): boolean;
 
-    setId(value: string): void;
-    setCatId(value: string): void;
-    setName(value: string): void;
-    setDescription(value: string): void;
-    setImage(value: string): void;
-    setPrice(value: number): void;
-    setStock(value: number): void;
-    setFeatured(value: boolean): void;
-    setActive(value: boolean): void;
+    // setId(value: string): void;
+    // setCatId(value: string): void;
+    // setName(value: string): void;
+    // setDescription(value: string): void;
+    // setImage(value: string): void;
+    // setPrice(value: number): void;
+    // setStock(value: number): void;
+    // setFeatured(value: boolean): void;
+    // setActive(value: boolean): void;
     }
 
 export class Product implements IProduct{
-    id: string;
-    catId: string;
+    id: number;
+    catId: number;
     name: string;
     description: string;
     image: string;
@@ -41,59 +41,59 @@ export class Product implements IProduct{
     featured: boolean;
     active: boolean;
 
-    getId(): string{
-        return(this.id);
-    };
-    getCatId(): string{
-        return(this.catId);
-    };
-    getName(): string{
-        return(this.name);
-    };
-    getDescription(): string{
-        return(this.description);
-    };
-    getImage(): string{
-        return(this.image);
-    };
-    getPrice(): number{
-        return(this.price);
-    };
-    getStock(): number{
-        return(this.stock);
-    };
-    isFeatured(): boolean{
-        return(this.featured);
-    };
-    isActive(): boolean{
-        return(this.active);
-    };
+    // getId(): string{
+    //     return(this.id);
+    // };
+    // getCatId(): string{
+    //     return(this.catId);
+    // };
+    // getName(): string{
+    //     return(this.name);
+    // };
+    // getDescription(): string{
+    //     return(this.description);
+    // };
+    // getImage(): string{
+    //     return(this.image);
+    // };
+    // getPrice(): number{
+    //     return(this.price);
+    // };
+    // getStock(): number{
+    //     return(this.stock);
+    // };
+    // isFeatured(): boolean{
+    //     return(this.featured);
+    // };
+    // isActive(): boolean{
+    //     return(this.active);
+    // };
 
-    setId(value: string): void{
-        this.id = value;
-    };
-    setCatId(value: string): void{
-        this.catId = value;
-    };
-    setName(value: string): void{
-        this.name = value;
-    };
-    setDescription(value: string): void{
-        this.description = value;
-    };
-    setImage(value: string): void{
-        this.image = value;
-    };
-    setPrice(value: number): void{
-        this.price = value;
-    };
-    setStock(value: number): void{
-        this.stock = value;
-    };
-    setFeatured(value: boolean): void{
-        this.featured = value;
-    };
-    setActive(value: boolean): void{
-        this.active = value;
-    };
+    // setId(value: string): void{
+    //     this.id = value;
+    // };
+    // setCatId(value: string): void{
+    //     this.catId = value;
+    // };
+    // setName(value: string): void{
+    //     this.name = value;
+    // };
+    // setDescription(value: string): void{
+    //     this.description = value;
+    // };
+    // setImage(value: string): void{
+    //     this.image = value;
+    // };
+    // setPrice(value: number): void{
+    //     this.price = value;
+    // };
+    // setStock(value: number): void{
+    //     this.stock = value;
+    // };
+    // setFeatured(value: boolean): void{
+    //     this.featured = value;
+    // };
+    // setActive(value: boolean): void{
+    //     this.active = value;
+    // };
 }
