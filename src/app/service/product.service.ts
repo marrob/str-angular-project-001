8acc9a8e90c6e63971d14f729920baac70bfffe6
Margit Robert <marrob50hz@gmail.com>
2021-01-31 15:11:15 +0100

    frissítve a kaegóriák száma a szervizben

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -7,7 +7,7 @@ import { Product } from '../model/Product';
 export class ProductService {
     list: Product[] = [
         { id: 0, name: "Ulrich Tietze -Christoph Schenk", description: "Cím: Analóg és digitális áramkörök, Év: 1981, Kiadó: Műszaki Könyvkiadó", catId: 10, image:"assets/img/termesszettudomany-villamosmernok/biliczsandor-amatematikavillamosmernokialkalmazasairolpeldakonkeresztul.jpg", price: 10, stock: 5, featured: false, active: true },
-        { id: 1, name: "Bálint Ágnes", description: "Cím: Szeleburdi család, Év: 1968, Kiadó: Móra", catId: 1, image: "assets/img/gyerek/balintagnes-szeleburdicsalad.jpg", price: 10, stock: 5, featured: false, active: true },
+        { id: 1, name: "Bálint Ágnes", description: "Cím: Szeleburdi család, Év: 1968, Kiadó: Móra", catId:1 , image: "assets/img/gyerek/balintagnes-szeleburdicsalad.jpg", price: 10, stock: 5, featured: false, active: true },
         { id: 2, name: "Benedek Elek", description: "Cím: Összes meséi III-IV., Év: 2007, Kiadó: Szukits Kiadó", catId: 1, image: "assets/img/gyerek/benedekelek-osszesmesei.jpg", price: 10, stock: 5, featured: false, active: true },
         { id: 3, name: "Fekete István", description: "Cím: Vuk, Év: 2009, Kiadó: Móra", catId: 1, image: "assets/img/gyerek/fekete-istvan-vuk.jpg", price: 10, stock: 5, featured: false, active: true },
         { id: 4, name: "Lázár Ervin", description: "Cím: A kisfiú meg az oroszlánok, Év: 1999, Kiadó: Osiris Kiadó", catId: 1, image: "assets/img/gyerek/lazarervin-akisfiumegazoroszlanok.jpg", price: 10, stock: 5, featured: false, active: true },
